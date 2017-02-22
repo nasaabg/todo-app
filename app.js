e3cc99014ddeeae4c6dc64f786dcc093766592ff
Janek Kurzydlo <janek.kurzydlo@gmail.com>
2017-02-22 22:50:14 +0100

    Update modules and app.js

--- app.js
+++ app.js
@@ -1,45 +1,51 @@
-/**
- * Module dependencies.
- */
-
 var express = require('express');
 var routes = require('./routes');
 var tasks = require('./routes/tasks');
 var http = require('http');
 var path = require('path');
 var mongoskin = require('mongoskin');
-
-// Initialize connection once
-var db = mongoskin.db('mongodb://localhost:27017/todo');
+var db = mongoskin.db('mongodb://localhost:27017/todo?auto_reconnect', {safe:true});
 var app = express();
+
+var favicon = require('serve-favicon'),
+  logger = require('morgan'),
+  bodyParser = require('body-parser'),
+  methodOverride = require('method-override'),
+  cookieParser = require('cookie-parser'),
+  session = require('express-session'),
+  csrf = require('csurf'),
+  errorHandler = require('errorhandler');
+
 app.use(function(req, res, next) {
   req.db = {};
   req.db.tasks = db.collection('tasks');
   next();
 })
 app.locals.appname = 'Express.js Todo App'
-// all environments
+app.locals.moment = require('moment');
 
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-// app.use(express.favicon());
-// app.use(express.logger('dev'));
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: false}));
-// app.use(express.methodOverride());
-var cookieParser = require('cookie-parser')
-app.use(cookieParser())
-// app.use(express.session({secret: '59B93087-78BC-4EB9-993A-A61FC844F6C9'}));
-// app.use(express.csrf());
+app.use(favicon(path.join('public','favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride());
+app.use(cookieParser('CEAF3FA4-F385-49AA-8FE4-54766A9874F1'));
+app.use(session({
+  secret: '59B93087-78BC-4EB9-993A-A61FC844F6C9',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(csrf());
 
-app.use(require('less-middleware')({ src: __dirname + '/public', compress: true }));
+app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
-
-var errorhandler = require('errorhandler')
-if ('development' == app.get('env')) {
-  app.use(errorhandler());
-}
+app.use(function(req, res, next) {
+  res.locals._csrf = req.csrfToken();
+  return next();
+})
 
 app.param('task_id', function(req, res, next, taskId) {
   req.db.tasks.findById(taskId, function(error, task){
@@ -55,12 +61,16 @@ app.get('/tasks', tasks.list);
 app.post('/tasks', tasks.markAllCompleted)
 app.post('/tasks', tasks.add);
 app.post('/tasks/:task_id', tasks.markCompleted);
-app.del('/tasks/:task_id', tasks.del);
+app.delete('/tasks/:task_id', tasks.del);
 app.get('/tasks/completed', tasks.completed);
 
 app.all('*', function(req, res){
-  res.send(404);
+  res.status(404).send();
 })
+// development only
+if ('development' == app.get('env')) {
+  app.use(errorHandler());
+}
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,5 +1,10 @@
-exports.list = function (req, res, next) {
-  req.db.tasks.find({completed: false}).toArray(function (error, tasks) {
+
+/*
+ * GET users listing.
+ */
+
+exports.list = function(req, res, next){
+  req.db.tasks.find({completed: false}).toArray(function(error, tasks){
     if (error) return next(error);
     res.render('tasks', {
       title: 'Todo List',
@@ -8,12 +13,13 @@ exports.list = function (req, res, next) {
   });
 };
 
-exports.add = function (req, res, next) {
+exports.add = function(req, res, next){
   if (!req.body || !req.body.name) return next(new Error('No data provided.'));
   req.db.tasks.save({
     name: req.body.name,
+    createTime: new Date(),
     completed: false
-  }, function (error, task) {
+  }, function(error, task){
     if (error) return next(error);
     if (!task) return next(new Error('Failed to save.'));
     console.info('Added %s with id=%s', task.name, task._id);
@@ -21,23 +27,22 @@ exports.add = function (req, res, next) {
   })
 };
 
-exports.markAllCompleted = function (req, res, next) {
+exports.markAllCompleted = function(req, res, next) {
   if (!req.body.all_done || req.body.all_done !== 'true') return next();
   req.db.tasks.update({
     completed: false
-  }, {
-    $set: {
-      completed: true
-    }
-  }, {multi: true}, function (error, count) {
+  }, {$set: {
+    completeTime: new Date(),
+    completed: true
+  }}, {multi: true}, function(error, count){
     if (error) return next(error);
     console.info('Marked %s task(s) completed.', count);
     res.redirect('/tasks');
   })
 };
 
-exports.completed = function (req, res, next) {
-  req.db.tasks.find({completed: true}).toArray(function (error, tasks) {
+exports.completed = function(req, res, next) {
+  req.db.tasks.find({completed: true}).toArray(function(error, tasks) {
     res.render('tasks_completed', {
       title: 'Completed',
       tasks: tasks || []
@@ -45,21 +50,22 @@ exports.completed = function (req, res, next) {
   });
 };
 
-exports.markCompleted = function (req, res, next) {
-  if (!req.body.completed) return next(new Error('Param is missing'));
-  req.db.tasks.updateById(req.task._id, {$set: {completed: req.body.completed === 'true'}}, function (error, count) {
+exports.markCompleted = function(req, res, next) {
+  if (!req.body.completed) return next(new Error('Param is missing.'));
+  var completed = req.body.completed === 'true';
+  req.db.tasks.updateById(req.task._id, {$set: {completeTime: completed ? new Date() : null, completed: completed}}, function(error, count) {
     if (error) return next(error);
-    if (count !== 1) return next(new Error('Something went wrong.'));
+    if (count !==1) return next(new Error('Something went wrong.'));
     console.info('Marked task %s with id=%s completed.', req.task.name, req.task._id);
     res.redirect('/tasks');
   })
 };
 
-exports.del = function (req, res, next) {
-  req.db.tasks.removeById(req.task._id, function (error, count) {
+exports.del = function(req, res, next) {
+  req.db.tasks.removeById(req.task._id, function(error, count) {
     if (error) return next(error);
-    if (count !== 1) return next(new Error('Something went wrong.'));
+    if (count !==1) return next(new Error('Something went wrong.'));
     console.info('Deleted task %s with id=%s completed.', req.task.name, req.task._id);
-    res.send(200);
+    res.status(204).send();
   });
 };
